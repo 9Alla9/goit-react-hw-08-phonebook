d50f9dcf8f4cd556b70944800716b739783491f3
9Alla9 <119464974+9Alla9@users.noreply.github.com>
2023-08-14 11:35:26 +0000

    Deploying to gh-pages from @ 9Alla9/goit-react-hw-08-phonebook@3e606457162e9bc8636feb1a5aebdbbe2407c031 🚀

---
+++ static/js/776.095f1eb8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[776],{4776:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var a=n(7689),c="Home_container__o2A3i",o="Home_text__h5Tq9",r=n(3329),s=function(){return(0,r.jsxs)("main",{className:c,children:[(0,r.jsx)("p",{className:o,children:"Your contact book"}),(0,r.jsx)(a.j3,{})]})}}}]);
+//# sourceMappingURL=776.095f1eb8.chunk.js.map
